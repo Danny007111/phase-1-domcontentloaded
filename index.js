3819494c025012c67919d339d95993cd74b760d4
Danny Rodriguez <danny007111@hotmail.com>
2022-04-26 13:47:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    callb();
+  });
+
+  function callb (){
+    let text1 = document.getElementById("text");
+    text1.innerHTML = "This is really cool!";
+  };
